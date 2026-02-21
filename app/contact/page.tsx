59f512c693f1842a3cc09a340655594d2e464595
Derek <dereksturm263@gmail.com>
2026-02-21 10:10:07 -0800

    test2

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
                   <Info />
                 </InputAdornment>
               ),
-           },
+            },
           }}
         />
       </main>
