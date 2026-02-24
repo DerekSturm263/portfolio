71a0adde0c85cd8d98518a1e27dbe5fa9d1b094b
Derek <dereksturm263@gmail.com>
2026-02-24 14:59:14 -0800

    a

--- app/lib/email.tsx
+++ app/lib/email.tsx
@@ -5,7 +5,7 @@ import emailjs from "@emailjs/browser";
 export default async function sendEmail(senderName: string, senderEmail: string, message: string) {
   const results = await emailjs.send(
     process.env.EMAILJS_SERVICE_ID ?? "",
-    process.env.EMALIJS_TEMPLATE_ID ?? "",
+    process.env.EMAILJS_TEMPLATE_ID ?? "",
     {
       title: `Inquiry From ${senderName}`,
       name: senderName,
