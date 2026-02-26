0883782554c984be3dcc089d68cdf888ff79e34e
Derek <dereksturm263@gmail.com>
2026-02-26 10:22:39 -0800

    test

--- app/lib/components.tsx
+++ app/lib/components.tsx
@@ -310,7 +310,7 @@ export function Section({ title, id, count, icon, allSortTags, allFilterTags, so
               value={sortDirection}
               onChange={(e) => setSortDirectionCallback(e.target.value)}
             >
-              {Object.keys(SortDirection).map((item, index) => (
+              {Object.values(SortDirection).filter(value => typeof value === "string").map((item, index) => (
                 <MenuItem
                   value={item}
                   key={index}
